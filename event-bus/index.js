b56c3d42b05f6d10d147b961111e02a18f61671e
kavishkamk <kavishkamadhushan727@gmail.com>
2022-10-09 12:30:35 +0530

    set depl and clusterip srv

--- event-bus/index.js
+++ event-bus/index.js
@@ -15,15 +15,15 @@ app.post("/events", (req, res) => {
     axios.post("http://post-cluserid-srv:4000/events", event).catch((err) => {
         console.log(err.message);
     });
-    // axios.post("http://localhost:4001/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    // axios.post("http://localhost:4002/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
-    // axios.post("http://localhost:4003/events", event).catch((err) => {
-    //     console.log(err.message);
-    // });
+    axios.post("http://comment-srv:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post("http://query-srv:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post("http://moderation-service-srv:4003/events", event).catch((err) => {
+        console.log(err.message);
+    });
     res.send({ status: "OK" });
 });
 
